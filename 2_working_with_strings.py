316500a9ea91c8df6ec161d1f5f40008cf62322e
Joce-Alva <jocealvarez8002@gmail.com>
2025-10-28 20:03:22 +0000

    working with strings

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -42,7 +42,11 @@ print("Length of phrase:", len(declaration_of_independence))
 # # ----------------------------------------
 # # 3. Indexing and Slicing
 # # ----------------------------------------
-
+chicago_mayor = "Johnson"
+print(len(chicago_mayor))
+print(chicago_mayor [0])
+print(chicago_mayor[4])
+print(chicago_mayor[-1])
 # # Indexing: Access characters by position (0-based index)
 # print("First character:", phrase[0])  # Output: P
 # print("Last character:", phrase[-1])  # Output: !
