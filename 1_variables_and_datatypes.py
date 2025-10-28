57a402670ee8698fe025605c818340a1bde41417
Joce-Alva <jocealvarez8002@gmail.com>
2025-10-28 19:29:48 +0000

    edits

--- 1_variables_and_datatypes.py
+++ 1_variables_and_datatypes.py
@@ -30,12 +30,12 @@ name = "John"
 print("Name:", name)
 
 # # 2. Numbers: Numeric data (integers or floats)
-# age = 23
-# print("Age:", age)
+age = 23
+print("Age:", age)
 
 # # 3. Booleans: True or False values
-# hungry = False
-# print("Hungry:", hungry)
+hungry = False
+print("Hungry:", hungry)
 
 
 # # Example combining them all:
